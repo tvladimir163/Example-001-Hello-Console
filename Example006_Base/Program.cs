762d1b2ece353da642d4136694d97c9e8874a0cf
v.tarasov <v.tarasov163@gmail.com>
2022-08-20 00:11:56 +0400

    Example 006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 2;
+int b = 4;
+int c = 8;
+int d = 6;
+int e = 5;
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
